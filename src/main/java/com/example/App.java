f7384c29708ff57e78474acb580fe94c54d404a0
Moonpearl <romain.tello@gmail.com>
2022-01-10 17:07:13 +0100

    Add user input management

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Point d'entrée de l'application
@@ -13,20 +14,53 @@ public class App
      */
     public static void main( String[] args )
     {
-        // Transforme un code de couleur hexadécimal en rouge, vert, bleu, et affiche le résultat
-        String hexString = "ffcc00";
-        int[] rgbArray = new int[] {
-            Integer.valueOf( hexString.substring( 0, 2 ), 16 ),
-            Integer.valueOf( hexString.substring( 2, 4 ), 16 ),
-            Integer.valueOf( hexString.substring( 4, 6 ), 16 ),
-        };
-        System.out.println( Arrays.toString(rgbArray) );
+        // Crée un gestionnaire qui permet d'écouter les saisies utilisateur dans la console
+        Scanner scanner = new Scanner(System.in);
 
-        // Transforme des valeurs de rouge, vert, bleu en code hexadécimal et affiche le résultat
-        int red = 255;
-        int green = 204;
-        int blue = 0;
-        hexString = String.format("#%02X%02X%02X", red, green, blue);
-        System.out.println( hexString );
+        // Attend une saisie de l'utilisateur
+        System.out.println("1. Entrer un code de couleur hexadécimal");
+        System.out.println("2. Entrer trois valeurs rouge, vert, bleu");
+        int choice = scanner.nextInt();
+
+        // En fonction de la valeur choisie par l'ûtilisateur
+        switch (choice) {
+            // Si c'est 1
+            case 1:
+                // Attend une saisie de l'utilisateur
+                System.out.println("Entrez un code de couleur hexadécimal");
+                scanner.nextLine();
+                String hexString = scanner.nextLine();
+
+                // Transforme un code de couleur hexadécimal en rouge, vert, bleu, et affiche le résultat    
+                int[] rgbArray = new int[] {
+                    Integer.valueOf( hexString.substring( 0, 2 ), 16 ),
+                    Integer.valueOf( hexString.substring( 2, 4 ), 16 ),
+                    Integer.valueOf( hexString.substring( 4, 6 ), 16 ),
+                };
+                System.out.println( Arrays.toString(rgbArray) );
+                break;
+
+            // Si c'est 2
+            case 2:
+                // Attend une saisie de l'utilisateur
+                System.out.println("Entrez une valeur de rouge:");
+                int red = scanner.nextInt();
+                // Attend une saisie de l'utilisateur
+                System.out.println("Entrez une valeur de vert:");
+                int green = scanner.nextInt();
+                // Attend une saisie de l'utilisateur
+                System.out.println("Entrez une valeur de bleu:");
+                int blue = scanner.nextInt();
+
+                // Transforme des valeurs de rouge, vert, bleu en code hexadécimal et affiche le résultat
+                hexString = String.format("#%02X%02X%02X", red, green, blue);
+                System.out.println( hexString );
+                break;
+
+            // Si ce n'est ni 1, ni 2
+            default:
+                System.out.println("Saisie invalide!");
+        }
     }
 }
+
